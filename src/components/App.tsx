dafc821560e8c96c7579e1d1ce12d9e81b814a7b
Maksym <mfedorenko16@gmail.com>
2019-12-01 14:50:28 -0500

    fetch and delete action

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Todo, fetchTodos } from '../actions';
 import { StoreState } from '../reducers/reducers';
+import { todosReducer } from '../reducers/todos';
 
 interface AppProps {
   todos: Todo[];
   fetchTodos(): any;
 }
 export class _App extends React.Component<AppProps> {
+  onClickHandler = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
+
   render() {
-    return <h1>Hello max</h1>;
+    return (
+      <div>
+        <button onClick={this.onClickHandler}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
